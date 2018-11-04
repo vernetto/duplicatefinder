6b4a085838468ceda3d5d7672e5a8f3828f5e000
vernetto <publicpierre@gmail.com>
2018-11-04 09:19:40 +0100

    adding

---
+++ src/test/java/org/pierre/duplicatefinder/MyTests.java
@@ -0,0 +1,10 @@
+package org.pierre.duplicatefinder;
+
+import org.junit.Test;
+
+public class MyTests {
+	@Test
+	public void whoami() {
+		System.out.println(System.getProperty("user.name"));
+	}
+}
